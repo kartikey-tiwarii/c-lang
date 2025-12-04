f00012bfb5abcefc270c297d4e15c9d5a0082889
Kartikey Tiwari <kartikeytiwari053@gmail.com>
2025-12-04 00:02:27 -0800

    print leap year using function

---
+++ leap_year__.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("enter n:");
+    scanf("%d", &n);
+    if (( n%4 == 0 && n%100 != 0) || n%400 == 0) {
+        printf("leap year:%d", n);
+    }
+    else {
+        printf("not a leap year:%d",n);
+    }
+}
+
