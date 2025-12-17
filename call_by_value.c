3591715445ab5ed87560a83e7cec8ea138165468
Kartikey Tiwari <kartikeytiwari053@gmail.com>
2025-12-16 17:03:55 -0800

    swapping value using call by value

---
+++ call_by_value.c
@@ -0,0 +1,19 @@
+// call by value (pointer)
+#include<stdio.h>
+int swap(int a, int b);
+int main() {
+    int x = 4;
+    int y = 5;
+    swap(x,y);
+    printf(" x:%d\n", x);
+    printf("y:%d \n", y);
+    return 0;
+}
+    int swap(int a, int b){
+    int c ;
+    c = b;
+    b = a;
+    a = c;
+    printf("a:%d \n", a);
+    printf("b:%d \n", b);
+    }
